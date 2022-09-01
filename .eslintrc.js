7de8ffe78cde435ec375ee18e5b623929ef8808c
kgtkr <contact@kgtkr.net>
2022-09-02 00:29:00 +0900

    lintとformatter設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,14 @@ module.exports = {
   env: {
     jest: true,
     node: true,
+    browser: true,
   },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:prettier/recommended",
     "prettier",
-    "prettier/@typescript-eslint",
   ],
   parserOptions: {
     project: "packages/*/tsconfig.json",
