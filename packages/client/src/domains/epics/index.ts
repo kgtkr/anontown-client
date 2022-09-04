e549ad2701c9ded9a4cbc8d1ef74d9b88381d4a8
kgtkr <contact@kgtkr.net>
2022-09-04 23:46:38 +0900

    Revert "redux依存削除"
    This reverts commit 0e38e8be2b444d4ef31ee4f2ac2fdd25cdd064da.
    
    ; Conflicts:
    ;	package-lock.json
    ;	packages/client/package.json
    ;	packages/client/src/main.tsx

---
+++ packages/client/src/domains/epics/index.ts
@@ -0,0 +1,6 @@
+import { combineEpics, Epic } from "redux-observable";
+import { RootAction } from "../actions";
+import { RootState } from "../state";
+
+export const epics: Epic<RootAction, RootAction, RootState, unknown> =
+  combineEpics();
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,20 +1,26 @@
 import { ApolloProvider } from "@apollo/react-common";
+import { ConnectedRouter } from "connected-react-router";
 import "core-js";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Modal from "react-modal";
+import { Provider } from "react-redux";
 import { App } from "./components/app";
+import { configureStore, history } from "./domains";
 import { gqlClient } from "./effects";
 import "./global.scss";
-import { BrowserRouter } from "react-router-dom";
 
 Modal.setAppElement("#root");
 
+const store = configureStore();
+
 ReactDOM.render(
   <ApolloProvider client={gqlClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>
   </ApolloProvider>,
   document.querySelector("#root")
 );
