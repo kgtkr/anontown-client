0e38e8be2b444d4ef31ee4f2ac2fdd25cdd064da
kgtkr <contact@kgtkr.net>
2022-09-04 19:52:13 +0900

    redux依存削除

--- packages/client/src/domains/epics/index.ts
+++
@@ -1,6 +0,0 @@
-import { combineEpics, Epic } from "redux-observable";
-import { RootAction } from "../actions";
-import { RootState } from "../state";
-
-export const epics: Epic<RootAction, RootAction, RootState, unknown> =
-  combineEpics();
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,30 +1,24 @@
 import { ApolloProvider } from "@apollo/react-common";
-import { ConnectedRouter } from "connected-react-router";
 import "core-js";
 import * as OfflinePluginRuntime from "offline-plugin/runtime";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Modal from "react-modal";
-import { Provider } from "react-redux";
 import { App } from "./components/app";
-import { configureStore, history } from "./domains";
 import { gqlClient } from "./effects";
 import "./global.scss";
+import { BrowserRouter } from "react-router-dom";
 
 Modal.setAppElement("#root");
 
 // Installing ServiceWorker
 OfflinePluginRuntime.install();
 
-const store = configureStore();
-
 ReactDOM.render(
   <ApolloProvider client={gqlClient}>
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <App />
-      </ConnectedRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ApolloProvider>,
   document.querySelector("#root")
 );
