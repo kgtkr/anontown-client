3a54fb0d4bb716dc76a5e73a6841021e439da98b
kgtkr <contact@kgtkr.net>
2022-09-04 23:29:21 +0900

    envの処理にバグがあったので修正

--- packages/client/src/env.ts
+++ packages/client/src/env.ts
@@ -1,9 +1,3 @@
 import * as env from "@anontown-frontend/common/dist/env";
 
-declare const __ENV__: env.Env | undefined;
-declare const __RAW_ENV__: Record<string, string | undefined> | undefined;
-
-export const Env =
-  typeof __ENV__ !== "undefined"
-    ? __ENV__
-    : env.loadEnv(typeof __RAW_ENV__ !== "undefined" ? __RAW_ENV__ : {});
+export const Env: env.Env = (globalThis as any).APP_ENV;
