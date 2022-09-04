f53b8536e6907be3d883f271933c5c091e3fa581
kgtkr <contact@kgtkr.net>
2022-09-04 23:27:38 +0900

    viteに移行(まだエラー出る)

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,6 +1,5 @@
 import { ApolloProvider } from "@apollo/react-common";
 import "core-js";
-import * as OfflinePluginRuntime from "offline-plugin/runtime";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Modal from "react-modal";
@@ -10,9 +9,6 @@ import "./global.scss";
 import { BrowserRouter } from "react-router-dom";
 
 Modal.setAppElement("#root");
-
-// Installing ServiceWorker
-OfflinePluginRuntime.install();
 
 ReactDOM.render(
   <ApolloProvider client={gqlClient}>
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,20 +1,35 @@
 import { defineConfig } from "vite";
 import * as path from "path";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import { ViteFaviconsPlugin } from "vite-plugin-favicon";
+import { loadEnv, htmlInject } from "@anontown-frontend/common/dist/env";
 
 export default defineConfig(({ command }) => {
   return {
     plugins: [
       react(),
-      /* command === "serve"
+      VitePWA(),
+      ViteFaviconsPlugin({
+        logo: "icon.svg",
+        inject: true,
+        favicons: {
+          appName: "Anontown",
+          appDescription: "高機能匿名掲示板Anontown",
+          background: "#006400",
+          theme_color: "#00ff00",
+          icons: {},
+        },
+      }),
+      command === "serve"
         ? (() => {
-            const appEnv = decodeAppEnv();
+            const appEnv = loadEnv(process.env);
             return {
               name: "appEnv-inject",
               transformIndexHtml: (html) => htmlInject(appEnv, html),
             };
           })()
-        : null,*/
+        : null,
     ],
     server: {
       port: 3000,
